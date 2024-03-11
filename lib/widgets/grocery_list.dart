a2d378c62dd3f897692d1539323662abe04399f9
Rizky Firman Syah <rizfirsy@gmail.com>
2024-03-11 21:53:56 +0700

    [FEAT] add new item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/widgets/new_item_screen.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item_screen.dart';
+
+import '../data/categories.dart';
+import '../models/category.dart';
+import '../models/grocery_item.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,28 +14,64 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context)
+  final List<GroceryItem> _groceryItems = [];
+
+  Future<void> _addItem() async {
+    var newItem = await Navigator.of(context)
         .push(MaterialPageRoute(builder: (ctx) => const NewItemScreen()));
+
+    if (newItem != null) {
+      setState(() {
+        _groceryItems.add(newItem);
+      });
+    } else {
+      return;
+    }
+  }
+  
+  void _removeItem(item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    var listItemContent =
+        const Center(child: Text("Your groceries stock is currently empty."));
+
+    if (_groceryItems.isNotEmpty) {
+      listItemContent = Center(
+        child: ListView.builder(
+          itemCount: _groceryItems.length,
+          itemBuilder: (context, index) => Dismissible(
+            key: ValueKey (_groceryItems[index].id),
+            direction: DismissDirection.horizontal,
+            background: Container(
+              color: Colors.redAccent,
+            ),
+            onDismissed: (direction) {
+              _removeItem(_groceryItems[index]);
+            },
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.color,
+              ),
+              trailing: Text(_groceryItems[index].quantity.toString()),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Groceries"),
       ),
-      body: ListView.builder(
-          itemCount: groceryItems.length,
-          itemBuilder: (context, index) => ListTile(
-                title: Text(groceryItems[index].name),
-                leading: Container(
-                  width: 24,
-                  height: 24,
-                  color: groceryItems[index].category.color,
-                ),
-                trailing: Text(groceryItems[index].quantity.toString()),
-              )),
+      body: listItemContent,
       floatingActionButton: FloatingActionButton(
         onPressed: _addItem,
         child: const Icon(Icons.add),
--- lib/widgets/new_item_screen.dart
+++ lib/widgets/new_item_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/category.dart';
+
+import '../models/grocery_item.dart';
 
 class NewItemScreen extends StatefulWidget {
   const NewItemScreen({super.key});
@@ -7,14 +12,133 @@ class NewItemScreen extends StatefulWidget {
   State<NewItemScreen> createState() {
     return _NewItemState();
   }
-
 }
 
 class _NewItemState extends State<NewItemScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Add new item")),
-      body: const Padding(padding: EdgeInsets.all(16), child: Text("Form"),),);
+  final _formKey = GlobalKey<FormState>();
+  var _titleState = '';
+  var _quantityState = 1;
+  var _categoryState = categories[Categories.vegetables]!;
+
+  void _saveState() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.of(context).pop(
+        GroceryItem(
+            id: DateTime.now().toString(),
+            name: _titleState,
+            quantity: _quantityState,
+            category: _categoryState),
+      );
+    }
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Add new item")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(label: Text("Title")),
+                maxLength: 50,
+                autofocus: true,
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 3 ||
+                      value.trim().length > 50) {
+                    return "Gaboleh kosong anjay";
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  _titleState = value;
+                },
+                initialValue: _titleState,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(label: Text("Quantity")),
+                      initialValue: _quantityState.toString(),
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return "Must be valid positif number.";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _quantityState = int.parse(value);
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      value: _categoryState,
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                              value: category.value,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 24,
+                                    height: 24,
+                                    color: category.value.color,
+                                  ),
+                                  const SizedBox(width: 24),
+                                  Text(category.value.title)
+                                ],
+                              ))
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _categoryState = value!;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveState,
+                    child: const Text('Add'),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
